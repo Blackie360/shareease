26df2ab7838e3f1d28faa63ca36d742690d67cdc
blackie360 <felixkent360@gmail.com>
2025-01-16 17:37:07 +0300

    fix(app): deleted some files

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,11 +4,16 @@ import AuthButtons from "@/components/auth-buttons";
 
 export default function Navbar() {
 	return (
-		<nav className="flex justify-between items-center py-3 px-4 fixed top-0 left-0 right-0 z-50 bg-slate-100">
-			<Link href="/" className="text-xl font-bold">
-				hymn
+		<nav className="flex justify-between items-center py-4 px-6 fixed top-0 left-0 right-0 z-50 bg-gradient-to-r from-orange-100 via-white to-pink-100 shadow-md">
+			{/* Logo */}
+			<Link href="/" className="text-2xl font-extrabold text-gray-800 hover:text-orange-600 transition-colors">
+				hymn<span className="text-pink-500">.</span>
 			</Link>
-			<AuthButtons />
+
+			{/* Auth Buttons */}
+			<div>
+				<AuthButtons />
+			</div>
 		</nav>
 	);
 }
